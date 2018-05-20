dff1b7f5bfffdce72d32583b30362ea3a4049d6b
Stanley Louis <stanlouis@msn.com>
2018-05-20 13:32:51 -0400

    Add winLostStatus function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,4 @@
 $ (function () {
-  function getRandomInt (min, max) {
-    min = Math.ceil (min);
-    max = Math.floor (max);
-    return Math.floor (Math.random () * (max - min)) + min;
-  }
   //Global variables
   var numberToGet = getRandomInt (50, 101);
   var blueNumber = getRandomInt (1, 15);
@@ -14,6 +9,19 @@ $ (function () {
   var looses = 0;
   var result = 0;
 
+  //Functions
+  function winLostStatus (totalResult) {
+    if (totalResult === numberToGet) {
+      console.log ('you win');
+      win++;
+      $ ('h2 span#winsNumber').text (wins);
+    } else if (totalResult > numberToGet) {
+      console.log ('You loose');
+      looses++;
+      $ ('h2 span#winsNumber').text (wins);
+    }
+  }
+
   $ ('h2 span#generatedNumber').text (numberToGet);
   $ ('h2 span#winsNumber').text (wins);
   $ ('h2 span#lossesNumber').text (looses);
@@ -22,17 +30,21 @@ $ (function () {
     .on ('click', '#blue', function () {
       result += blueNumber;
       console.log (result);
+      winLostStatus (result);
     })
     .on ('click', '#green', function () {
       result += greenNumber;
       console.log (result);
+      winLostStatus (result);
     })
     .on ('click', '#red', function () {
       result += redNumber;
       console.log (result);
+      winLostStatus (result);
     })
     .on ('click', '#yellow', function () {
       result += yellowNumber;
       console.log (result);
+      winLostStatus (result);
     });
 });
